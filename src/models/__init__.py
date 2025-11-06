bf911266f13c106d7b50af5fb68e90b57962d943
guillermo nihito <gufilgueira@nihito.io>
2025-11-06 22:02:37 +0100

    a

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,6 @@
 from .BaseModel import BaseModel
 from .ConvLSTM_Guillermo_v1 import ConvLSTM_Guillermo_v1
+from .ConvLSTM_Guillermo_v2 import ConvLSTM_Guillermo_v2
 from .ConvLSTMLightning import ConvLSTMLightning
 from .EvaluationMetrics import MyTestMetrics
 from .LogisticRegression import LogisticRegression
